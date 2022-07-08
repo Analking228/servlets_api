c78c1788a3f6d38c9865c88a483e6dcdab4a265c
analking228 <kronos5525@yandex.ru>
2022-07-08 20:42:51 +0300

    added log adn lofmapper class || TODO: add another table in data sql for logs of users

--- src/main/java/edu/school/cinema/models/Log.java
+++ src/main/java/edu/school/cinema/models/Log.java
@@ -1,4 +1,57 @@
 package edu.school.cinema.models;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 public class Log {
+    private int     userId;
+    private String  email;
+    private String  date;
+    private String  time;
+
+    Log(){}
+
+    Log(int userId, String email){
+        this.userId = userId;
+        this.email = email;
+        Date now = new Date();
+        DateFormat formatterTime = new SimpleDateFormat("HH:mm:ss");
+        DateFormat formatterDate = new SimpleDateFormat("yyyy-MM-dd");
+        time = formatterTime.format(now);
+        date = formatterDate.format(now);
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
--- src/main/java/edu/school/cinema/models/LogMapper.java
+++ src/main/java/edu/school/cinema/models/LogMapper.java
@@ -1,4 +1,18 @@
 package edu.school.cinema.models;
 
-public class LogMapper {
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LogMapper implements RowMapper<Log> {
+    @Override
+    public Log mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Log log = new Log();
+        log.setUserId(rs.getInt("user_id"));
+        log.setEmail(rs.getString("email"));
+        log.setDate(rs.getString("date"));
+        log.setTime(rs.getString("time"));
+        return log;
+    }
 }
