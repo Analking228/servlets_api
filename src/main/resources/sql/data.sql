498c925c93dab8e1e3eba17f50c7820a6afce3f8
analking228 <kronos5525@yandex.ru>
2022-07-10 21:35:00 +0300

    added log adn lofmapper class || TODO: add another table in data sql for logs of users

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO cinema.users(first_name, last_name, phone_number, email, password)
 values('Alex', 'Mc Fucker', '+79647940209', 'loler228@yandex.ru', '$2a$10$/1c5idFMQF9aZqro.LljquecZS2NXOiFjt/OF6d5jKPImkiAAKFVO');
+
+INSERT INTO cinema.logs (email, ip)
+values('loler228@yandex.ru', '89171111111');
--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,7 +1,9 @@
-DROP SCHEMA IF EXISTS cinema CASCADE ;
+DROP SCHEMA IF EXISTS cinema CASCADE;
 CREATE SCHEMA IF NOT EXISTS cinema;
 
 DROP TABLE IF EXISTS cinema.users;
+DROP TABLE IF EXISTS cinema.logs;
+
 CREATE TABLE IF NOT EXISTS cinema.users(
     user_id SERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
@@ -9,5 +11,12 @@ CREATE TABLE IF NOT EXISTS cinema.users(
     phone_number VARCHAR(100) NOT NULL,
     email   VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL
+);
+create table IF NOT EXISTS cinema.logs
+(
+    log_id SERIAL PRIMARY KEY,
+    email VARCHAR(100) NOT NULL ,
+    ip VARCHAR(100) NOT NULL,
+    date timestamp not null default now()
 );
 
